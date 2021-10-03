0b41c576e3a8d0efef5fb74b8a96729ec3562da2
nichols <a.nichols.taylor@gmail.com>
2021-10-03 16:28:03 -0600

    Added serial lib, began implementing moteus comms, commented out currently unused joint

--- src/grarm/src/hwinterface.h
+++ src/grarm/src/hwinterface.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <controller_manager/controller_manager.h>
 #include <ros/ros.h>
-
+#include <CppLinuxSerial/SerialPort.hpp>
 
 class MyRobot : public hardware_interface::RobotHW
 {
@@ -13,8 +13,8 @@ public:
   MyRobot() 
  { 
    // connect and register the joint state interface
-   hardware_interface::JointStateHandle state_handle_1("joint1", &pos[0], &vel[0], &eff[0]);
-   jnt_state_interface.registerHandle(state_handle_1);
+   //hardware_interface::JointStateHandle state_handle_1("joint1", &pos[0], &vel[0], &eff[0]);
+   //jnt_state_interface.registerHandle(state_handle_1);
 
    hardware_interface::JointStateHandle state_handle_2("joint2", &pos[1], &vel[1], &eff[1]);
    jnt_state_interface.registerHandle(state_handle_2);
@@ -34,8 +34,8 @@ public:
    registerInterface(&jnt_state_interface);
 
    // connect and register the joint position interface
-   hardware_interface::PosVelJointHandle pos_vel_handle_1(jnt_state_interface.getHandle("joint1"), &cmd_pos[0], &cmd_vel[0]);
-   jnt_pos_vel_interface.registerHandle(pos_vel_handle_1);
+   //hardware_interface::PosVelJointHandle pos_vel_handle_1(jnt_state_interface.getHandle("joint1"), &cmd_pos[0], &cmd_vel[0]);
+   //jnt_pos_vel_interface.registerHandle(pos_vel_handle_1);
 
    hardware_interface::PosVelJointHandle pos_vel_handle_2(jnt_state_interface.getHandle("joint2"), &cmd_pos[1], &cmd_vel[1]);
    jnt_pos_vel_interface.registerHandle(pos_vel_handle_2);
@@ -54,32 +54,201 @@ public:
 
    registerInterface(&jnt_pos_vel_interface);
 
-   
+   mn::CppLinuxSerial::SerialPort serialPortA("/dev/ttyUSB0", mn::CppLinuxSerial::BaudRate::B_9600);
+   serialPort.SetTimeout(-1);
+   serialPort.Open();
+
+   // mn::CppLinuxSerial::SerialPort serialPortB("/dev/ttyUSB0", mn::CppLinuxSerial::BaudRate::B_9600);
+   // serialPort.SetTimeout(-1);
+   // serialPort.Open();
  }
 
   void write(){
-    std::cout<<"Writing Positions: " << cmd_pos[0] << ", " << cmd_pos[1] << ", " << cmd_pos[2] << "\n";
-    std::cout<<"Writing Velocities: " << cmd_vel[0] << ", " << cmd_vel[1] << ", " << cmd_vel[2] << "\n";
+    // std::cout<<"Writing Positions: " << cmd_pos[0] << ", " << cmd_pos[1] << ", " << cmd_pos[2] << "\n";
+    // std::cout<<"Writing Velocities: " << cmd_vel[0] << ", " << cmd_vel[1] << ", " << cmd_vel[2] << "\n";
+
+    //For each joint in serial unit,
+    
+    //Get commanded position,
+    //Adjust units, compensate for gearbox, and LSB Val
+    //Convert to hex, LSB first
+    //Get commanded velocity,
+    //Adjust units, compensate for gearbox and LSB val
+    //Convert to hex, LSB first
+    //Build then send command
+    serialPortA.Write("can send 8001 ...");
+
+    // Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above)
+    std::string readData;
+    serialPortA.Read(readData);
+
+    // For given set of pos/vel/torque
+    // Convert from lsb first to regular hex
+    // convert to dec,
+    // adjust for gearbox and lsb val
+    // store
   }
    
-  void read(){
-    std::cout<<"Reading.\n";
-  }
+  // void read(){
+  //   std::cout<<"Reading.\n";
+  // }
 
 private:
   hardware_interface::JointStateInterface jnt_state_interface;
   hardware_interface::PosVelJointInterface jnt_pos_vel_interface;
-  double cmd_pos[2];
-  double cmd_vel[2];
-  double pos[2];
-  double vel[2];
-  double eff[2];
+  double cmd_pos[6];
+  double cmd_vel[6];
+  double pos[6];
+  double vel[6];
+  double eff[6];
+  mn::CppLinuxSerial::SerialPort serialPortA;
+  // mn::CppLinuxSerial::SerialPort serialPortB
+  ;
 };
 
+std::string DecToHex(long long dec)
+{
+	long long n = dec;
+	// algorithm from geeks for geeks: https://www.geeksforgeeks.org/program-decimal-hexadecimal-conversion/
+	
+	// convert dec 
+
+	// convert to 2's complement form
+	if (n < 0L)
+	{
+		n = n + 4294967296L;
+	}
+
+	// char array to store hexadecimal number 
+	char hexaDeciNum[100];
+
+	// counter for hexadecimal number array 
+	int i = 0;
+	while (n != 0)
+	{
+		// temporary variable to store remainder 
+		int temp = 0;
+
+		// storing remainder in temp variable. 
+		temp = n % 16;
+
+		// check if temp < 10 
+		if (temp < 10)
+		{
+			hexaDeciNum[i] = temp + 48;
+			i++;
+		}
+		else
+		{
+			hexaDeciNum[i] = temp + 55;
+			i++;
+		}
+
+		n = n / 16;
+	}
+
+	std::string finalString = "";
+
+	// printing hexadecimal number array in reverse order 
+	for (int j = i - 1; j >= 0; j--)
+	{
+		finalString += hexaDeciNum[j];
+	}
+
+	if (finalString.size() < 8)
+	{
+		for (; finalString.size() - 8 > 0; )
+		{
+			finalString = finalString + '0';
+		}
+	}
+
+	if (n < 0L)
+	{
+		std::reverse(finalString.begin(), finalString.end());
+	}
+
+	// swaps the chars
+
+	for (int l = 0; l < 8; l++)
+	{
+		if (l % 2 == 0)
+		{
+			char temp = finalString[l];
+			finalString[l] = finalString[l + 1];
+			finalString[l + 1] = temp;
+		}
+	}
+
+	return finalString;
+}
+
+int HexCharToDecVal(char hexChar)
+{
+	switch (hexChar)
+	{
+	case ('0'):
+		return 0;
+	case ('1'):
+		return 1;
+	case ('2'):
+		return 2;
+	case ('3'):
+		return 3;
+	case ('4'):
+		return 4;
+	case ('5'):
+		return 5;
+	case ('6'):
+		return 6;
+	case ('7'):
+		return 7;
+	case ('8'):
+		return 8;
+	case ('9'):
+		return 9;
+	case ('A'):
+		return 10;
+	case ('B'):
+		return 11;
+	case ('C'):
+		return 12;
+	case ('D'):
+		return 13;
+	case ('E'):
+		return 14;
+	case ('F'):
+		return 15;
+	default:
+		return 0;
+	}
+}
+
+long long HexToDec(std::string hexString)
+{
+	int stringSize = hexString.length();
+	long long total = 0;
+	int j = 0;
+	for (int i = stringSize - 1; i >= 0; i--)
+	{
+	  char currentChar = hexString[j];
+	  total += pow(16, i) * HexCharToDecVal(currentChar);
+	  j++;
+	}
+
+	// for 8 chars
+	// convert from 2s complement -1
+	if (total > 2147483648L)
+	{
+		total -= 4294967296L;
+	}
+
+	return total;
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "my_grarm");
+  ros::init(argc, argv, "grarm_hw_interface");
 
   MyRobot robot;
   controller_manager::ControllerManager cm(&robot);
@@ -95,7 +264,7 @@ int main(int argc, char **argv)
     const ros::Time time = ros::Time::now();
     const ros::Duration period = time - prev_time;
     
-    robot.read();
+    // robot.read();
     cm.update(time,period);
     robot.write();
     rate.sleep();
